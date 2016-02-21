97b1f2ea04f7c624572ec87be6d323edc58e7822
Sebastien Lemery <seblemery@gmail.com>
2016-02-21 13:32:07 -0500

    Update freenode.tcl
    fixed !unban and !unmute to accept <manual hosts>

--- freenode.tcl
+++ freenode.tcl
@@ -12,7 +12,7 @@ proc op_and_ban {nick uhost handle chan text} {
 proc op_and_unban {nick uhost handle chan text} {
 	global botnick
 	putserv "privmsg ChanServ :op $chan $botnick"
-	utimer 1 [list putserv "mode $chan -bo [maskhost [lindex [split $text] 0]![getchanhost [lindex [split $text] 0] $chan] 2] $botnick " ]
+	utimer 1 [list putserv "mode $chan -bo [lindex [split $text] 0] $botnick " ]
 }
 
 proc op_and_mute {nick uhost handle chan text} {
@@ -24,5 +24,5 @@ proc op_and_mute {nick uhost handle chan text} {
 proc op_and_unmute {nick uhost handle chan text} {
 	global botnick
 	putserv "privmsg ChanServ :op $chan $botnick"
-	utimer 1 [list putserv "mode $chan -qo [maskhost [lindex [split $text] 0]![getchanhost [lindex [split $text] 0] $chan] 2] $botnick " ]
+	utimer 1 [list putserv "mode $chan -qo [lindex [split $text] 0] $botnick " ]
 }
